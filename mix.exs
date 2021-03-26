1abae1515cebd25b169b7edba0e5afee84c574d4
Geovane Guibes <geovane.guibes@sigacred.com.br>
2021-03-26 17:20:33 -0300

    Add linters in the project

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule Bankapi.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 
