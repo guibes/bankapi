920aa6d10caaafe9ae78ce0429fb8afc792b4400
Geovane Guibes <geovane.guibes@sigacred.com.br>
2021-03-26 13:13:27 -0300

    Configurations for Gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :bankapi, BankapiWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  server: true,
+  http: [port: {:system, "PORT"}],
+  url: [host:  System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
