63ddae0973fc6b9b0ab6480e46fb034b579c0db5
Geovane Guibes <geovane.guibes@sigacred.com.br>
2021-03-28 08:10:36 -0300

    Create or update module

--- lib/user/create_or_update.ex
+++ lib/user/create_or_update.ex
@@ -4,6 +4,11 @@ defmodule Bankapi.User.CreateorUpdate do
   """
   alias Bankapi.{Repo, User}
 
+  @doc """
+  Function to create or update a user in the database using Repo.
+  Case the referral code not exists or user status of code is pending, return errors.
+  """
+
   def create_or_update(params) do
     %{"cpf" => cpf} = params
 
@@ -11,6 +16,27 @@ defmodule Bankapi.User.CreateorUpdate do
       nil -> User.changeset(params)
       %User{} = user -> User.update_changeset(user, params)
     end
+    |> validate_referral_code()
     |> Repo.insert_or_update()
+  end
+
+  defp validate_referral_code(changeset) do
+    case changeset.changes do
+      %{referral_code: referral_code} -> handle_referral(changeset, referral_code)
+      %{} -> changeset
+    end
+  end
+
+  defp handle_referral(changeset, referral_code) do
+    case Repo.get_by(User, user_code: referral_code) do
+      nil ->
+        Ecto.Changeset.add_error(changeset, :referral_code, "invalid referral_code")
+
+      %User{status: "pending"} ->
+        Ecto.Changeset.add_error(changeset, :referral_code, "user of referral_code is pending")
+
+      _ ->
+        changeset
+    end
   end
 end
