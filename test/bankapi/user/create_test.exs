ef7025222425ce01339a8a503930104affa9a61b
Geovane Guibes <geovane.guibes@sigacred.com.br>
2021-03-30 16:37:01 -0300

    Created some tests

---
+++ test/bankapi/user/create_test.exs
@@ -0,0 +1,97 @@
+defmodule Bankapi.User.CreateTest do
+  use Bankapi.DataCase
+  alias Bankapi.User.Create
+  alias Bankapi.{User, Repo}
+
+  @valid_attrs_complete %{
+      cpf: "12345678934",
+      password: "12345678912",
+      country: "USA",
+      city: "Vitorino",
+      birth_date: "2000-01-01",
+      state: "Paraná",
+      name: "Teste Teste",
+      email: "tutaaa@tsaa.com"
+  }
+
+  @valid_attrs_pending %{
+      cpf: "12345678934",
+      password: "12345678912"
+  }
+
+  @valid_attrs_referral_wrong %{
+      cpf: "12345678934",
+      password: "12345678912",
+      country: "USA",
+      city: "Vitorino",
+      birth_date: "2000-01-01",
+      state: "Paraná",
+      name: "Teste Teste",
+      referral_code: "abcdefgh",
+      email: "tutaaa@tsaa.com"
+  }
+
+  describe "call/1" do
+    test "when all params are valid return an user with complete status" do
+
+      {:ok, %User{id: user_id, user_code: user_code, password_hash: password_hash}} = Create.call(@valid_attrs_complete)
+
+      user = Repo.get(User, user_id)
+      assert %User{
+        id: ^user_id,
+        birth_date: ~D[2000-01-01],
+        city: "Vitorino",
+        country: "USA",
+        cpf: "12345678934",
+        email: "tutaaa@tsaa.com",
+        gender: nil,
+        name: "Teste Teste",
+        password: nil, #because is virtual
+        password_hash: ^password_hash,
+        referral_code: nil,
+        state: "Paraná",
+        status: "complete",
+        user_code: ^user_code
+        } = user
+    end
+
+    test "when all params are valid and referral_code user is complete" do
+      {:ok, %User{user_code: user_code}} = Create.call(@valid_attrs_complete)
+      new_user = %{
+          cpf: "12345678935",
+          password: "12345678912",
+          country: "USA",
+          city: "Vitorino",
+          birth_date: "2000-01-01",
+          state: "Paraná",
+          name: "Teste Teste",
+          email: "aaaa@aaaa.com",
+          referral_code: user_code
+      }
+      {:ok, new_user_response} = Create.call(new_user)
+      assert %User{referral_code: ^user_code} = new_user_response
+    end
+
+    test "when all params are valid but referral_code is invalid" do
+      {:error, changeset} = Create.call(@valid_attrs_referral_wrong)
+
+      assert %{referral_code: ["invalid referral_code"]} = errors_on(changeset)
+      refute changeset.valid?
+    end
+
+    test "when all params are valid but user is pending" do
+      {:ok, %User{user_code: user_code}} = Create.call(@valid_attrs_pending)
+      new_user = %{
+          cpf: "12345678935",
+          password: "12345678912",
+          referral_code: user_code
+      }
+      {:error, changeset} = Create.call(new_user)
+      assert %{referral_code: ["user of referral_code is pending"]} = errors_on(changeset)
+      refute changeset.valid?
+    end
+
+
+  end
+
+end
---
+++ test/bankapi_web/controllers/user_controller_test.exs
@@ -0,0 +1,294 @@
+defmodule BankapiWeb.UserControllerTest do
+  use BankapiWeb.ConnCase
+  import Plug.BasicAuth
+
+  @valid_attrs_complete %{
+      cpf: "12345678934",
+      password: "12345678912",
+      country: "USA",
+      city: "Vitorino",
+      birth_date: "2000-01-01",
+      state: "Paraná",
+      name: "Teste Teste",
+      email: "tutaaa@tsaa.com"
+  }
+
+  @valid_attrs_complete_another_user %{
+      cpf: "12345678935",
+      password: "12345678912",
+      country: "Brasil",
+      city: "Florianópolis",
+      birth_date: "2000-01-01",
+      state: "Santa Catarina",
+      name: "Teste Teste",
+      email: "aaa@aaa.com"
+  }
+
+  @valid_attrs_incomplete %{
+    cpf: "12345678935",
+    password: "12345678912",
+    country: "Brasil",
+    city: "Florianópolis",
+  }
+
+  @invalid_attrs %{
+      cpf: "1234567893",
+      password: "123456",
+      country: "Brasil",
+      city: "Florianópolis",
+      birth_date: "2000-01-01",
+      state: "Santa Catarina",
+      name: "Teste Teste",
+      email: "aaaaaa.com"
+  }
+
+  describe "create_or_update/2" do
+    test "POST /api/user Creating a new user", %{conn: conn} do
+      %Plug.Conn{resp_body: resp_body} = post(conn, "/api/user", @valid_attrs_complete)
+      {:ok, %{"user" => %{"id" => id, "user_code" => user_code}} = response} = Jason.decode(resp_body)
+
+      assert %{
+        "message" => "User register complete",
+        "user" => %{
+          "birth_date" => "2000-01-01",
+          "city" => "Vitorino",
+          "country" => "USA",
+          "cpf" => "12345678934",
+          "email" => "tutaaa@tsaa.com",
+          "gender" => nil,
+          "id" => ^id,
+          "name" => "Teste Teste",
+          "referral_code" => nil,
+          "state" => "Paraná",
+          "status" => "complete",
+          "user_code" => ^user_code
+        }
+      } = response
+    end
+
+    test "POST /api/user Creating a new user with incomplete data", %{conn: conn} do
+      %Plug.Conn{resp_body: resp_body} = post(conn, "/api/user", @valid_attrs_incomplete)
+      {:ok, %{"user" => %{"id" => id}} = response} = Jason.decode(resp_body)
+
+
+      assert %{
+        "message" => "User register pending",
+        "user" => %{
+          "birth_date" => nil,
+          "city" => "Florianópolis",
+          "country" => "Brasil",
+          "cpf" => "12345678935",
+          "email" => nil,
+          "gender" => nil,
+          "id" => ^id,
+          "name" => nil,
+          "referral_code" => nil,
+          "state" => nil,
+          "status" => "pending"
+        }
+      } = response
+    end
+
+    test "POST /api/user Creating a new user with invalid data", %{conn: conn} do
+      %Plug.Conn{resp_body: resp_body} = post(conn, "/api/user", @invalid_attrs)
+      {:ok, response} = Jason.decode(resp_body)
+
+      assert %{
+        "message" => %{"cpf" =>
+        ["has invalid format", "should be at least 11 character(s)"],
+        "email" => ["has invalid format"],
+        "password" => ["should be at least 8 character(s)"]}
+      } = response
+    end
+
+    test "POST /api/user Updating a user", %{conn: conn} do
+
+      %Plug.Conn{} = post(conn, "/api/user", @valid_attrs_complete)
+
+      new_params = %{
+          cpf: "12345678934",
+          password: "12345678912",
+          country: "Brasil",
+          city: "São Paulo",
+          birth_date: "2000-01-01",
+          state: "São Paulo",
+          name: "Teste Teste",
+          email: "tutaaa@tsaa.com"
+      }
+
+      conn = put_req_header(conn, "authorization", encode_basic_auth("12345678934", "12345678912"))
+
+      %Plug.Conn{resp_body: resp_body} = post(conn, "/api/user", new_params)
+      {:ok, %{"user" => %{"id" => id, "user_code" => user_code}} = response} = Jason.decode(resp_body)
+      assert %{
+        "message" => "User register complete",
+        "user" => %{
+          "birth_date" => "2000-01-01",
+          "city" => "São Paulo",
+          "country" => "Brasil",
+          "cpf" => "12345678934",
+          "email" => "tutaaa@tsaa.com",
+          "gender" => nil,
+          "id" => ^id,
+          "name" => "Teste Teste",
+          "referral_code" => nil,
+          "state" => "São Paulo",
+          "status" => "complete",
+          "user_code" => ^user_code
+        }
+      } = response
+    end
+
+    test "POST /api/user Updating a user with incomplete data", %{conn: conn} do
+
+      %Plug.Conn{} = post(conn, "/api/user", @valid_attrs_incomplete)
+
+      new_params = %{
+          cpf: "12345678935",
+          password: "12345678912",
+          country: "Brasil",
+          city: "São Paulo",
+          gender: "Prefiro não declarar",
+          birth_date: "2000-01-01",
+          state: "São Paulo",
+          name: "Teste Teste",
+          email: "tutaaa@tsaa.com"
+      }
+
+      conn = put_req_header(conn, "authorization", encode_basic_auth("12345678935", "12345678912"))
+
+      %Plug.Conn{resp_body: resp_body} = post(conn, "/api/user", new_params)
+      {:ok, %{"user" => %{"id" => id, "user_code" => user_code}} = response} = Jason.decode(resp_body)
+      assert %{
+        "message" => "User register complete",
+        "user" => %{
+          "birth_date" => "2000-01-01",
+          "city" => "São Paulo",
+          "country" => "Brasil",
+          "cpf" => "12345678935",
+          "email" => "tutaaa@tsaa.com",
+          "gender" => "Prefiro não declarar",
+          "id" => ^id,
+          "name" => "Teste Teste",
+          "referral_code" => nil,
+          "state" => "São Paulo",
+          "status" => "complete",
+          "user_code" => ^user_code
+        }
+      } = response
+    end
+
+    test "POST /api/user Updating a user but with wrong cpf", %{conn: conn} do
+
+      %Plug.Conn{} = post(conn, "/api/user", @valid_attrs_complete)
+
+      new_params = %{
+          cpf: "12345678934",
+          password: "12345678912",
+          country: "Brasil",
+          city: "São Paulo",
+          birth_date: "2000-01-01",
+          state: "São Paulo",
+          name: "Teste Teste",
+          email: "tutaaa@tsaa.com"
+      }
+
+      conn = put_req_header(conn, "authorization", encode_basic_auth("1234567893", "12345678912"))
+
+      %Plug.Conn{resp_body: resp_body} = post(conn, "/api/user", new_params)
+      {:ok,  response} = Jason.decode(resp_body)
+      assert %{"message" => %{"error" => "User not found"}} = response
+    end
+
+    test "POST /api/user Updating a user but with another valid user/password", %{conn: conn} do
+      %Plug.Conn{} = post(conn, "/api/user", @valid_attrs_complete)
+      %Plug.Conn{} = post(conn, "/api/user", @valid_attrs_complete_another_user)
+
+      new_params = %{
+          cpf: "12345678934",
+          password: "12345678912",
+          country: "Brasil",
+          city: "São Paulo",
+          birth_date: "2000-01-01",
+          state: "São Paulo",
+          name: "Teste Teste",
+          email: "tutaaa@tsaa.com"
+      }
+
+      conn = put_req_header(conn, "authorization", encode_basic_auth("12345678935", "12345678912"))
+
+      %Plug.Conn{resp_body: resp_body} = post(conn, "/api/user", new_params)
+      {:ok, response} = Jason.decode(resp_body)
+      assert  %{"message" => %{"error" => "Do not have permission"}} = response
+    end
+  end
+
+  describe "show_referrals/2" do
+    test "GET /api/user/:user_code/referrals Getting a referrals list", %{conn: conn} do
+      %Plug.Conn{resp_body: resp_body} = post(conn, "/api/user", @valid_attrs_complete)
+      {:ok, %{"user" => %{"user_code" => user_code}}} = Jason.decode(resp_body)
+
+      another_user = %{
+          cpf: "12345678935",
+          password: "12345678912",
+          country: "Brasil",
+          city: "São Paulo",
+          birth_date: "2000-01-01",
+          state: "São Paulo",
+          name: "Teste Teste",
+          email: "aaa@tsaa.com",
+          referral_code: user_code
+      }
+
+      post(conn, "/api/user", another_user)
+
+
+      conn = put_req_header(conn, "authorization", encode_basic_auth("12345678934", "12345678912"))
+
+      %Plug.Conn{resp_body: resp_body} = get(conn, "/api/user/"<> user_code <>"/referrals")
+      {:ok, response} = Jason.decode(resp_body)
+
+      assert %{
+        "message" => "Listing all referrals",
+        "referrals" => _
+      } = response
+    end
+
+    test "GET /api/user/:user_code/referrals Getting a referrals list empty", %{conn: conn} do
+      %Plug.Conn{resp_body: resp_body} = post(conn, "/api/user", @valid_attrs_complete)
+      {:ok, %{"user" => %{"user_code" => user_code}}} = Jason.decode(resp_body)
+      conn = put_req_header(conn, "authorization", encode_basic_auth("12345678934", "12345678912"))
+      %Plug.Conn{resp_body: resp_body} = get(conn, "/api/user/"<> user_code <>"/referrals")
+      {:ok, response} = Jason.decode(resp_body)
+
+      assert %{"message" => %{"error" => "User has no referrals"}} = response
+    end
+
+    test "GET /api/user/:user_code/referrals Getting a referrals list with another username", %{conn: conn} do
+      %Plug.Conn{resp_body: resp_body} = post(conn, "/api/user", @valid_attrs_complete)
+      {:ok, %{"user" => %{"user_code" => user_code}}} = Jason.decode(resp_body)
+
+      another_user = %{
+          cpf: "12345678935",
+          password: "12345678912",
+          country: "Brasil",
+          city: "São Paulo",
+          birth_date: "2000-01-01",
+          state: "São Paulo",
+          name: "Teste Teste",
+          email: "aaa@tsaa.com",
+          referral_code: user_code
+      }
+
+      post(conn, "/api/user", another_user)
+
+
+      conn = put_req_header(conn, "authorization", encode_basic_auth("12345678935", "12345678912"))
+
+      %Plug.Conn{resp_body: resp_body} = get(conn, "/api/user/"<> user_code <>"/referrals")
+      {:ok, response} = Jason.decode(resp_body)
+
+      assert %{"message" => %{"error" => "Do not have permission"}} = response
+    end
+  end
+end
