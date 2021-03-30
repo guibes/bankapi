eb603fad791212e02ab77a572535b98e012012c3
Geovane Guibes <geovane.guibes@sigacred.com.br>
2021-03-30 08:39:13 -0300

    Fallback Controller Update

--- lib/bankapi_web/controllers/fallback_controller.ex
+++ lib/bankapi_web/controllers/fallback_controller.ex
@@ -1,6 +1,27 @@
 defmodule BankapiWeb.FallbackController do
   use BankapiWeb, :controller
 
+  def call(conn, {:forbidden, result}) do
+    conn
+    |> put_status(:forbidden)
+    |> put_view(BankapiWeb.ErrorView)
+    |> render("403.json", result: result)
+  end
+
+  def call(conn, {:unauthorized, result}) do
+    conn
+    |> put_status(:unauthorized)
+    |> put_view(BankapiWeb.ErrorView)
+    |> render("401.json", result: result)
+  end
+
+  def call(conn, {:not_found, result}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(BankapiWeb.ErrorView)
+    |> render("404.json", result: result)
+  end
+
   def call(conn, {:error, result}) do
     conn
     |> put_status(:bad_request)
